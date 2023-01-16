f04ccac1d669c33b88f6cdfadb470b0c25035dfe
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-01-16 16:17:43 -0300

    Update CopyPipeline.groovy

--- CopyPipeline.groovy
+++ CopyPipeline.groovy
@@ -10,16 +10,9 @@ pipeline {
             steps {
 
                 script{
-                    find = powershell (returnStdout:true, script: "Invoke-Command -ComputerName 192.168.200.131 -ScriptBlock {Test-Path -Path C:\\FTP\\inbox\\*}").trim()
 
-                    echo find 
+                    powershell (returnStdout:true, script: "Invoke-Command -ComputerName 192.168.200.131 -ScriptBlock {Test-Path -Path C:\\FTP\\inbox\\*}").trim()
 
-                    if(find == "True"){
-                        echo "True"
-                    }
-                    else{
-                        echo "False"
-                    }
                 }
             }
         }
