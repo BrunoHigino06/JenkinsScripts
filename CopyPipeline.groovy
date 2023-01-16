f24f196d4df1fd677cff96b2c99a580ecf81743b
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-01-16 16:01:52 -0300

    Create CopyPipeline.groovy

---
+++ CopyPipeline.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+
+    environment{
+        def find = ''
+    }
+
+    stages {
+        stage('Copy') {
+            steps {
+
+                script{
+                    find = powershell (returnStdout:true, script: "Invoke-Command -ComputerName 192.168.200.131 -ScriptBlock {Test-Path -Path C:\\FTP\\inbox\\*}").trim()
+
+                    echo find 
+
+                    if(find == "True"){
+                        echo "True"
+                    }
+                    else{
+                        echo "False"
+                    }
+                }
+            }
+        }
+    }
+}
