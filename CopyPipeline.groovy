75566d7e2d272e08002c12053beefbd85815100d
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-01-16 17:29:29 -0300

    Update CopyPipeline.groovy

--- CopyPipeline.groovy
+++ CopyPipeline.groovy
@@ -13,6 +13,8 @@ pipeline {
 
                     file = powershell (returnStdout:true, script: "Invoke-Command -ComputerName 192.168.200.131 -ScriptBlock {Get-ChildItem -Path C:\\FTP\\inbox\\ | select Name}").trim()
 
+                    powershell (returnStdout:true, script: "Invoke-Command -ComputerName 192.168.200.131 -ScriptBlock {...}")
+
                 }
             }
         }
