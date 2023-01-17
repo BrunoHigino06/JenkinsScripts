4b1961dda4ab06fd75b44ae6a13e145172195466
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-01-17 20:21:06 -0300

    Update CopyPipeline.groovy

--- CopyPipeline.groovy
+++ CopyPipeline.groovy
@@ -14,9 +14,16 @@ pipeline {
                     find = powershell (returnStdout:true, script: "{Test-Path -Path \\\\192.168.200.131\\ftp\\*}").trim()
 
                     if(find == "True"){
+                        
+                        if(){
 
-                        file = powershell (returnStdout:true, script: "Get-ChildItem -Path \\\\192.168.200.131\\ftp\\ | select Name").trim()
-                        powershell (returnStdout:true, script: "Copy-Item '\\\\192.168.200.131\\ftp\\${file}' -Destination '\\\\192.168.200.132\\inbox\\'")    
+                            file = powershell (returnStdout:true, script: "Get-ChildItem -Path \\\\192.168.200.131\\ftp\\ | select Name").trim()
+                            powershell (returnStdout:true, script: "Copy-Item '\\\\192.168.200.131\\ftp\\${file}' -Destination '\\\\192.168.200.132\\inbox\\'")
+
+                        }
+                        else{
+
+                        } 
 
                     }
                     else{
