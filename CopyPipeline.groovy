8ae70663e73862ab55e9903574e0814686d67f37
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-01-17 14:51:00 -0300

    Update CopyPipeline.groovy

--- CopyPipeline.groovy
+++ CopyPipeline.groovy
@@ -12,15 +12,15 @@ pipeline {
                 script{
 
                     try{
-                    file = powershell (returnStdout:true, script: "Get-ChildItem -Path \\\\192.168.200.131\\ftp\\ | select Name").trim()
-                    powershell (returnStdout:true, script: "Copy-Item '\\\\192.168.200.131\\ftp\\${file}' -Destination '\\\\192.168.200.132\\ftp\\inbox\\'")                        
-                    
-                    }catch(Exception e){
+                        file = powershell (returnStdout:true, script: "Get-ChildItem -Path \\\\192.168.200.131\\ftp\\ | select Name").trim()
+                        powershell (returnStdout:true, script: "Copy-Item '\\\\192.168.200.131\\ftp\\${file}' -Destination '\\\\192.168.200.132\\ftp\\inbox\\'")                        
 
                     }
+                    
+                    catch(Exception e){
+                        echo 'Error to copy the file to the deployment server'
 
-
-
+                    }
                 }
             }
         }
