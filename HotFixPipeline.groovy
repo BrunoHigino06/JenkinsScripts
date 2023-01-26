f1c0692db21589e470acf40f2d5c9888d3404d3f
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-01-26 17:05:53 -0300

    Update HotFixPipeline.groovy

--- HotFixPipeline.groovy
+++ HotFixPipeline.groovy
@@ -4,11 +4,13 @@ pipeline {
     stages {
         
         stage("Start the Deployment") {
-            steps {
-                script {
-                    env.DevServers.tokenize(",").each { server ->
-                        stage(server){
-                            echo "Server is $server in parallel"
+            parallel {
+                steps {
+                    script {
+                        env.DevServers.tokenize(",").each { server ->
+                            stage(server){
+                                echo "Server is $server in parallel"
+                            }
                         }
                     }
                 }
