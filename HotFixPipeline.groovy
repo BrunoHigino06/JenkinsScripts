df072e81753a8c2b896b19215fcfe11ca942769d
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-01-26 14:47:11 -0300

    Update HotFixPipeline.groovy

--- HotFixPipeline.groovy
+++ HotFixPipeline.groovy
@@ -7,12 +7,9 @@ pipeline {
             steps {
                 script {
                     env.DevServers.tokenize(",").each { server ->
-                        for(int i=0; i < server.size(); i++) {
-                            stage(server){
-                                echo "Server is $server"
-                            }
+                        stage(server){
+                            echo "Server is $server"
                         }
-                        echo "Server is $server"
                     }
                 }
             }
